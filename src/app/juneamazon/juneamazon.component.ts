514411dc55f2941a0a00162cfa3ac6ccb0386962
wilsonmike <michaelwilsonsj@hotmail.com>
2021-07-09 12:49:48 -0400

    juneamazon and junecapes finished all

--- src/app/juneamazon/juneamazon.component.ts
+++ src/app/juneamazon/juneamazon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import juneamazondata from '../juneamazon.json';
 
 @Component({
   selector: 'app-juneamazon',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./juneamazon.component.css']
 })
 export class JuneamazonComponent implements OnInit {
+  juneAmazon: any = juneamazondata;
+  term = '';
 
   constructor() { }
 
--- src/app/junecapes/junecapes.component.ts
+++ src/app/junecapes/junecapes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import junecapesdata from '../junecapesfetch.json';
 
 @Component({
   selector: 'app-junecapes',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./junecapes.component.css']
 })
 export class JunecapesComponent implements OnInit {
+  juneCapes: any = junecapesdata;
+  term = '';
 
   constructor() { }
 
